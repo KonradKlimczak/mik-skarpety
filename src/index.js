6c024f9d4391b04b49a47081f472abb25229fd4b
Konrad <konradklimczak93@gmail.com>
2017-09-02 13:38:44 +0200

    Burger menu added.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import '../node_modules/font-awesome/css/font-awesome.css';
-import './style.css';
+import '../node_modules/font-awesome/scss/font-awesome.scss';
+import './global.scss';
+
+const navbar = document.querySelector('.mik-navbar');
+const burgerIcon = document.querySelector('.burger-icon');
+
+burgerIcon.addEventListener('click', () => navbar.classList.toggle('is-active'))
