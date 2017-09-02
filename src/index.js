1811a81a4f15d7d620c4a6577c68b332d09e8591
Konrad <konradklimczak93@gmail.com>
2017-09-02 16:35:08 +0200

    Desktop navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import '../node_modules/font-awesome/scss/font-awesome.scss';
 import './global.scss';
 
+const categories = ['Nowość', 'Best seller', 'Męskie', 'Damskie', 'Dziecięce']
+
 const navbar = document.querySelector('.mik-navbar');
 const burgerIcon = document.querySelector('.burger-icon');
+const mikMenu = document.querySelector('.mik-menu');
 
 burgerIcon.addEventListener('click', () => navbar.classList.toggle('is-active'))
+
+mikMenu.innerHTML = categories
+  .map(category => `
+  <li class="mik-menu-item">
+    ${category}
+  </li>
+  `)
+  .join('');
