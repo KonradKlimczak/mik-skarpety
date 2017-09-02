ff01ae26921cb5fd63499f14bf9d690b894dd856
Konrad <konradklimczak93@gmail.com>
2017-09-02 16:58:05 +0200

    search added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import '../node_modules/font-awesome/scss/font-awesome.scss';
 import './global.scss';
 
 const categories = ['Nowość', 'Best seller', 'Męskie', 'Damskie', 'Dziecięce']
+const products = [{
+  category: 'Męskie',
+  name: 'Skarpety garniturowe'
+}, {
+  category: 'Męskie',
+  name: 'Skarpety sportowe'
+}, {
+  category: 'Damskie',
+  name: 'Skarpety w kwiaty'
+}]
+
 
 const navbar = document.querySelector('.mik-navbar');
 const burgerIcon = document.querySelector('.burger-icon');
@@ -16,3 +27,31 @@ mikMenu.innerHTML = categories
   </li>
   `)
   .join('');
+
+function findMatches(wordToMatch, ciproductsties) {
+  return products.filter(product => {
+    // here we need to figure out if the city or state matches what was searched
+    const regex = new RegExp(wordToMatch, 'gi');
+    return product.category.match(regex) || product.name.match(regex)
+  });
+}
+
+function displayMatches() {
+  if (this.value === '') {
+    suggestions.innerHTML = '';
+    return;
+  }
+  const matchArray = findMatches(this.value, products);
+  suggestions.innerHTML = matchArray
+    .map(product => `
+      <li>
+        <span class="name">${product.name}</span>
+      </li>
+    `)
+    .join('');
+}
+const searchInput = document.getElementById('search-offer');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
