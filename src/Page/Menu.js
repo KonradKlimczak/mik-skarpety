2d9b3e777b8b79f7446edc0a1e4a10203127807b
Konrad <konradklimczak93@gmail.com>
2017-09-04 16:26:17 +0200

    `createDocumentFragment` used

--- src/Page/Menu.js
+++ src/Page/Menu.js
@@ -9,13 +9,15 @@ const mikMenu = document.querySelector('.mik-menu');
 burgerIcon.addEventListener('click', () => navbar.classList.toggle('is-active'));
 
 export default () => {
+  const menuItems = document.createDocumentFragment();
   categories.map(category => {
     const menuItem = document.createElement('li');
     menuItem.setAttribute('class', 'mik-menu-item');
     menuItem.setAttribute('data-url', category.url);
     menuItem.textContent = category.label;
-    mikMenu.appendChild(menuItem);
+    menuItems.appendChild(menuItem);
   });
+  mikMenu.appendChild(menuItems);
 
   mikMenu.addEventListener('click', (e) => {
     if (!e.target.matches('li')) return; // skip this unless it's an input
